093b3a639d20a1f77618b3552c6e224893ea801d
Michael Chen <chen.3221@osu.edu>
2016-04-05 21:15:39 -0400

    Added js to open reportcard in new tab

--- src/js/popup.js
+++ src/js/popup.js
@@ -15,3 +15,16 @@ chrome.extension.sendMessage({}, function(response) {
 		}
 	}, 10);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+});
