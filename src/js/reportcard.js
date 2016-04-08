85a5d801309e73f58e651cd1f0cfc2109fc1bf59
Michael Chen <chen.3221@osu.edu>
2016-04-08 10:48:59 -0400

    Query fix didn't go through for some reason

--- src/js/reportcard.js
+++ src/js/reportcard.js
@@ -1,17 +1,16 @@
 var table = document.createElement('table');
 chrome.history.search({text: '', maxResults: 10}, function(data) {
     data.forEach(function(page) {
-    	var host = getLocation(currentTab.url);
+    	var host = page.url;
     	var tr = document.createElement('tr');
     	var td1 = document.createElement('td');
     	var td2 = document.createElement('td');
-    	var text1 = document.createTextNode(host.hostname);
+    	var text1 = document.createTextNode(host);
     	var xmlhttp = new XMLHttpRequest();
     	xmlhttp.onreadystatechange = function() {
    		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var myArr = JSON.parse(xmlhttp.responseText);
-        var categories = myArr[host.hostname]["categories"];
-        trustworthiness = myArr[host.hostname]["0"][0];
+        var trustworthiness = myArr[host.hostname]["0"][0];
          $('#trustworthiness').html(trustworthiness);
           if(trustworthiness >= 60){
                     $('#trustworthiness').addClass("label-success");
@@ -25,7 +24,7 @@ chrome.history.search({text: '', maxResults: 10}, function(data) {
         td2.appendChild(text2);
    		 }
 		};
-    	xmlhttp.open("GET", "http://api.mywot.com/0.4/public_link_json2?hosts="+host.hostname+"&key=aac42146ef84f207eda6922c397768d57043c5f5", true);
+    	xmlhttp.open("GET", "http://api.mywot.com/0.4/public_link_json2?hosts="+"www.google.com"+"/&key=aac42146ef84f207eda6922c397768d57043c5f5", true);
     	xmlhttp.send();
     	$('#hostname').html(host.hostname);
 		td1.appendChild(text1);
