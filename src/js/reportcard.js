813ce67b7bb61f7fa73d1cf1e7fb07747762f646
Michael Chen <chen.3221@osu.edu>
2016-04-08 13:55:51 -0400

    Fixed hostname and json stuff

--- src/js/reportcard.js
+++ src/js/reportcard.js
@@ -1,11 +1,11 @@
 var table = document.createElement('table');
 chrome.history.search({text: '', maxResults: 10}, function(data) {
     data.forEach(function(page) {
-    	var host = page.url;
+    	var host = getLocation(page.url);
     	var tr = document.createElement('tr');
     	var td1 = document.createElement('td');
     	var td2 = document.createElement('td');
-    	var text1 = document.createTextNode(host);
+    	var text1 = document.createTextNode(host.hostname);
     	var xmlhttp = new XMLHttpRequest();
     	xmlhttp.onreadystatechange = function() {
    		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -19,12 +19,11 @@ chrome.history.search({text: '', maxResults: 10}, function(data) {
                 } else {
                     $('#trustworthiness').addClass("label-danger");
                 }
-                $('#categories').html(listCategories(categories));
         var text2 = document.createTextNode(myArr[host.hostname]["0"][0]);
         td2.appendChild(text2);
    		 }
 		};
-    	xmlhttp.open("GET", "http://api.mywot.com/0.4/public_link_json2?hosts="+"www.google.com"+"/&key=aac42146ef84f207eda6922c397768d57043c5f5", true);
+    	xmlhttp.open("GET", "http://api.mywot.com/0.4/public_link_json2?hosts="+host.hostname+"/&key=aac42146ef84f207eda6922c397768d57043c5f5", true);
     	xmlhttp.send();
     	$('#hostname').html(host.hostname);
 		td1.appendChild(text1);
@@ -34,3 +33,9 @@ chrome.history.search({text: '', maxResults: 10}, function(data) {
     });
     document.body.appendChild(table);
 });
+
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
