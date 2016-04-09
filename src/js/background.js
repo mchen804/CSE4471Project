da32b0fa629e4e1a118dfd0f97c5b4bbdb41437c
Ankai Lou <ankailou@gmail.com>
2016-04-09 14:08:14 -0400

    update badge to change with tab change. not that it makes a big difference either way.

--- src/js/background.js
+++ src/js/background.js
@@ -72,3 +72,40 @@ var eat = function(tab) {
   });
 };
 
+chrome.tabs.onActivated.addListener( function( tabId ){
+  // load current whitelist on update/create
+  chrome.storage.sync.get(['whitelist'], function(result) {
+    var whitelist = result.whitelist ? result.whitelist : [];
+    console.log(whitelist);
+    // remove all associated cookies
+    chrome.tabs.get( tabId.tabId, function(tab) {
+      chrome.cookies.getAll({"url" : tab.url}, function(cookies) {
+        if (cookieMonsterStatus && cookies.length > 0) {
+          cookiesByDomain = cookies.map( function(cookie) { return cookie.domain; });
+          cookiePageEaten = 0;
+          cookiePageTotal = 0;
+          // iteratively delete non-whitelisted cookies
+          cookies.forEach( function(cookie) {
+            if (whitelist.indexOf(cookie.domain) === -1) {
+              var fqdn = getURL(cookie);
+              console.log(fqdn);
+              chrome.cookies.remove({ url : fqdn, name : cookie.name, storeId : cookie.storeId });
+              chrome.cookies.remove({ url : fqdn.replace('www.', ''), name : cookie.name, storeId : cookie.storeId });
+              cookiePageEaten++;
+              cookiePageTotal++;
+              cookiesSinceInstall++;
+            } else {
+              cookiePageTotal++;
+            }
+          });
+        } else {
+          cookiesByDomain = [];
+          cookiePageTotal = 0;
+          cookiePageEaten = 0;
+        }
+        chrome.browserAction.setBadgeText({ text : cookiePageEaten + '' });
+      });
+    });
+  });
+});
+
